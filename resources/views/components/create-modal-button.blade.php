021dd779aa4157691b37f0e72ccec388c5e80098
Ange Arsene <nkenmandenga@gmail.com>
2025-06-12 10:40:04 +0100

    feat: Update create modal for product addition with relevant fields and CSRF token

--- resources/views/components/create-modal-button.blade.php
+++ resources/views/components/create-modal-button.blade.php
@@ -6,41 +6,31 @@
         <!-- Modal -->
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
-                <form class="modal-content" action="" method="POST">
+                <form class="modal-content" action="" method="POST" id="createCustomerForm">
 
                     @csrf
 
                     @method('post')
 
                     <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Add New Customer</h5>
+                        <h5 class="modal-title" id="staticBackdropLabel">Add New Product</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row mb-3">
                             <div class="col">
-                                <label for="first_name" class="form-label">First Name</label>
-                                <input type="text" class="form-control" id="first_name" name="first_name" required>
+                                <label for="product_name" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" id="product_name" name="product_name" required>
                             </div>
                             <div class="col">
-                                <label for="last_name" class="form-label">Last Name</label>
-                                <input type="text" class="form-control" id="last_name" name="last_name" required>
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" required>
+                                <label for="quantity" class="form-label">Quantity in stock</label>
+                                <input type="number" class="form-control" id="quantity" name="quantity" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col">
-                                <label for="gender" class="form-label">Gender</label>
-                                <select class="form-select" id="gender" name="gender" required>
-                                    <option value="">Select</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                </select>
+                                <label for="price" class="form-label">Price per item in (USD)</label>
+                                <input type="number" class="form-control" id="price" name="price" required>
                             </div>
                         </div>
                     </div>
