8c50d5d883b74a08b639d0df894ffa6f851cf6b4
Ange Arsene <nkenmandenga@gmail.com>
2025-06-12 15:02:02 +0100

    feat: Add edit modal form for product updates with dynamic rendering

--- resources/js/ajax/app.ts
+++ resources/js/ajax/app.ts
@@ -17,8 +17,11 @@ window.onload = async () => {
 
     products.map((product: Product) => {
         totalInventory += product.quantity * product.price;
+
         tableElem.innerHTML += formatTableRow(product);
         totalInventoryElem.innerHTML = `${totalInventory}`;
+
+        document.body.insertAdjacentHTML('beforeend', editModalForm(product));
     });
 }
 
@@ -71,7 +74,9 @@ const formatTableRow = (product: Product): string => {
             <td>${new Date(product.created_at).toLocaleDateString()}</td>
             <td>${product.quantity * product.price}</td>
             <td>
-                <a href="" class="btn btn-primary">Edit</a>
+                <button type="button" display="inline-block" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop${product.id}">
+                    Edit
+                </button>
                 <button type="submit" class="btn btn-danger">Delete</button>
             </td>
         </tr>
@@ -90,5 +95,48 @@ document.getElementById('createCustomerForm')?.addEventListener('submit', () => 
     saveSpinner.classList.remove('d-none');
     saveText.textContent = 'Saving...';
 });
+
+const editModalForm = (product: Product): string => {
+    return `
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop${product.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <form class="modal-content" action="" method="POST" id="createCustomerForm${product.id}">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Edit Product</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="name" class="form-label">Product Name</label>
+                                <input type="text" value="${product.name}" class="form-control" id="product_name" name="name" required>
+                            </div>
+                            <div class="col">
+                                <label for="quantity" class="form-label">Quantity in stock</label>
+                                <input type="number" value="${product.quantity}" class="form-control" id="quantity" name="quantity" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="price" class="form-label">Price per item in (USD)</label>
+                                <input type="number" value="${product.price}" class="form-control" id="price" name="price" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success" id="saveButton">
+                            <span id="saveText">Save</span>
+                            <span id="saveSpinner" class="spinner-border spinner-border-sm d-none" role="status"
+                                aria-hidden="true"></span>
+                        </button>
+
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>`;
+};
 
 console.log("Ajax script loaded");
