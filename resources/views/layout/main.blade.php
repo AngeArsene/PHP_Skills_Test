ca88c82287690cb9c3dc382ad584433c34c2eb00
Ange Arsene <nkenmandenga@gmail.com>
2025-06-12 14:02:45 +0100

    feat: Replace session success message with dynamic alert box for improved user feedback

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -21,12 +21,10 @@
         Product's Crud Appllication
     </nav>
 
-    @session('success')
-        <div class="container mb-3 alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>{{ session('success') }}</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endsession
+    <div class="container mb-3 alert alert-warning alert-dismissible fade show" role="alert" id="alert-box" style="display: none;">
+        <strong id="alert-message"></strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 
     @yield('content')
 
